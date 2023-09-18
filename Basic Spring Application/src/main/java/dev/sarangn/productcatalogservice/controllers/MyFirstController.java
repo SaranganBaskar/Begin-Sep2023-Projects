8c746c9131ff7f72dbd03a05e05a511ee3f8307f
Sarangan Baskar <sarangan.baskar@injazat.com>
2023-09-18 23:26:56 +0530

    Basic Spring Application Java

---
+++ Basic Spring Application/src/main/java/dev/sarangn/productcatalogservice/controllers/MyFirstController.java
@@ -0,0 +1,23 @@
+package dev.sarangn.productcatalogservice.controllers;
+
+import dev.sarangn.productcatalogservice.services.MyFirstService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyFirstController {
+    private MyFirstService myFirstService;
+
+
+    public MyFirstController(MyFirstService myFirstService) {
+        this.myFirstService = myFirstService;
+    }
+
+    @GetMapping("/")
+    public String SayHi(){
+        return "Hi There!! Welcome to Scaler!!!";
+    }
+
+
+}
---
+++ Basic Spring Application/src/main/java/dev/sarangn/productcatalogservice/services/Greeting.java
@@ -0,0 +1,3 @@
+package dev.sarangn.productcatalogservice.services;
+
+public record Greeting(long id, String content) { }
